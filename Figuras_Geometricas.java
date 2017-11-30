4afc49301ef402e10373a06ba76a64f8dc1f665f
Mayra <mauh1234@hotmail.com>
2017-11-30 10:39:50 -0500

    mis archivos de figuras geometricas

---
+++ Figuras_Geometricas.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figuras_geometricas;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author MAYRA
+ */
+public class Figuras_Geometricas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int rectangulo,codigo,base,altura;
+        double area;
+        codigo=Integer.parseInt(JOptionPane.showInputDialog("Digite el codigo de la figura \n 1.Rectangulo \n 2.Cuadrado \n 3.Circulo"));
+        if (codigo==1){
+        base=Integer.parseInt(JOptionPane.showInputDialog("Digite la base"));
+        altura=Integer.parseInt(JOptionPane.showInputDialog("Digite la altura"));
+        area=base*altura;
+        JOptionPane.showMessageDialog(null,"El area de un rectangulo es: "+area);
+        }
+
+    }
+    
+}
